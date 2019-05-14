93e0274c83e354a265a4cd4fe487572b9a04c62a
Jason Willwerscheid <willwerscheid@uchicago.edu>
2019-05-14 16:37:06 -0500

    Rename methods file and fix tests.

--- tests/testthat/test_zero_factors.R
+++ tests/testthat/test_zero_factors.R
@@ -25,7 +25,7 @@ test_that("sampler works correctly when there are zero factors", {
 })
 
 test_that("zero factors are dealt with correctly when normalizing loadings", {
-  expect_equal(f$loadings$scale.constant[1], 0)
-  expect_equal(f$loadings$normalized.loadings[[1]][, 1], rep(0, n))
-  expect_equal(f$loadings$normalized.loadings[[2]][, 1], rep(0, p))
+  expect_equal(f$diagonal[1], 0)
+  expect_equal(f$loadings[[1]][, 1], rep(0, n))
+  expect_equal(f$loadings[[2]][, 1], rep(0, p))
 })
