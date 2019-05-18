9362f1ba99952acf1dc97d82d06c4aa61c12f76c
Jason Willwerscheid <willwerscheid@uchicago.edu>
2019-05-18 11:38:08 -0500

    Add ops for low-rank matrices.

--- R/ops_nmode_products.R
+++ R/ops_nmode_products.R
@@ -1,9 +1,11 @@
 #   N-mode products generalize matrix-vector products and constitute the key
 # operations for this package. They currently work for matrices, sparse
-# matrices (as implemented by package Matrix), and 3-dimensional tensors
-# (which should be input as arrays). Ideally, they would also accept sparse
-# tensors and tensors of arbitrary dimension, but I am not aware of any good
-# R packages for handling these types of object.
+# matrices (as implemented by package Matrix), 3-dimensional tensors (which
+# should be input as arrays), and low-rank matrices and tensors (objects of
+# class "lowrank" which are stored as lists of matrices of dimension d_n x k).
+# Ideally, they would also accept sparse tensors and tensors of arbitrary
+# dimension, but I am not aware of any good R packages for handling these types
+# of object.
 
 nmode.ops.error <- paste("N-mode products are not yet implemented for",
                          "objects of that class and/or dimension.")
@@ -27,6 +29,16 @@ nmode.prod.vec <- function(X, v, n) {
       return(as.vector(X %*% v))
   }
 
+  # Low-rank matrices:
+  if (inherits(X, "lowrank") && length(X) == 2) {
+    if (identical(v, 1))
+      u <- colSums(X[[n]])
+    else
+      u <- t(v %*% X[[n]])
+
+    return(as.vector(X[[-n]] %*% u))
+  }
+
   # 3-dimensional tensors:
   if (is.array(X) && length(dim(X) == 3)) {
     if (n == 1 && identical(v, 1))
@@ -43,10 +55,24 @@ nmode.prod.vec <- function(X, v, n) {
       return(apply(X, 2, FUN = function(M) {M %*% v}))
   }
 
+  # Low-rank tensors (returns a low-rank matrix):
+  if (inherits(X, "lowrank") && length(X) == 3) {
+    if (identical(v, 1))
+      u <- colSums(X[[n]])
+    else
+      u <- as.vector(v %*% X[[n]])
+
+    retval <- X[-n]
+    smaller.dim <- which.min(c(nrow(retval[[1]]), nrow(retval[[2]])))
+    retval[[smaller.dim]] <- t(t(retval[[smaller.dim]]) * u)
+    class(retval) <- "lowrank"
+    return(retval)
+  }
+
   stop(nmode.ops.error)
 }
 
-# X is a m_1 x m_2 (x m_3) matrix (array), v is a r1 object whose dimension
+# X is a m_1 x m_2 (x m_3) matrix (array), r1 is a r1 object whose dimension
 #   is one less than X, and n indicates the dimension that is omitted. For
 #   matrices, this is simply the usual matrix product of X and the vector
 #   in r1. For tensors, it is the n-mode product of X and the rank-one matrix
@@ -55,52 +81,28 @@ nmode.prod.vec <- function(X, v, n) {
 #   Any of the list elements in r1 can be set to 1 to indicate a vector of all
 #   ones.
 #
-fullrank.nmode.prod.r1 <- function(X, r1, n) {
+nmode.prod.r1 <- function(X, r1, n) {
+  if (is.null(X))
+    return(0)
+
   if (identical(X, 1)) {
     return(r1.sum(r1))
   }
 
-  if (is.matrix(X) || inherits(X, "Matrix")) {
+  if (is.matrix(X)
+      || inherits(X, "Matrix")
+      || (inherits(X, "lowrank") && length(X) == 2)) {
     return(nmode.prod.vec(X, unlist(r1), (1:2)[-n]))
   }
 
-  if (is.array(X) && (length(dim(X)) == 3)) {
+  if ((is.array(X) && (length(dim(X)) == 3))
+      || (inherits(X, "lowrank") && length(X) == 3)) {
     ns <- (1:3)[-n]
     # Go backwards (otherwise indices get messed up).
     return(nmode.prod.vec(nmode.prod.vec(X, r1[[2]], ns[2]), r1[[1]], ns[1]))
   }
 
   stop(nmode.ops.error)
-}
-
-# The following function has the same purpose as nmode.prod.r1, except that
-#   it takes a lowrank object as an argument rather than a full matrix (or
-#   tensor). The idea is to avoid computing with a large m_1 x m_2 matrix (or
-#   m_1 x m_2 x m_3 tensor).
-#
-lowrank.nmode.prod.r1 <- function(lowrank, r1, n) {
-  # Matrices:
-  if (length(lowrank) == 2)
-    return(as.vector(lowrank[[n]]
-                     %*% nmode.prod.vec(lowrank[[(1:2)[-n]]], r1[[1]], 1)))
-
-  # 3-dimensional tensors:
-  if (length(lowrank) == 3) {
-    ns <- (1:3)[-n]
-    return(as.vector(lowrank[[n]]
-                     %*% (nmode.prod.vec(lowrank[[ns[1]]], r1[[1]], 1)
-                          * nmode.prod.vec(lowrank[[ns[2]]], r1[[2]], 1))))
-  }
-
-  stop(nmode.ops.error)
-}
-
-nmode.prod.r1 <- function(X, r1, n) {
-  if (is.null(X))
-    return(0)
-  if (inherits(X, "lowrank"))
-    return(lowrank.nmode.prod.r1(X, r1, n))
-  return(fullrank.nmode.prod.r1(X, r1, n))
 }
 
 # The following calculates the n-mode product between the matrix or tensor
@@ -110,19 +112,36 @@ nmode.prod.r1 <- function(X, r1, n) {
 #   general be sparse, simply performing elementwise multiplication will
 #   destroy sparsity in X).
 #
+premult.nmode.prod.r1 <- function(Z, X, r1, n) {
+  if (is.null(X))
+    return(0)
+
+  if (inherits(X, "lowrank"))
+    return(premult.lowrank.nmode.prod.r1(Z, X, r1, n))
+
+  if (inherits(Z, "lowrank"))
+    return(premult.lowrank.nmode.prod.r1(X, Z, r1, n))
+
+  return(nmode.prod.r1(Z * X, r1, n))
+}
+
 premult.lowrank.nmode.prod.r1 <- function(Z, lowrank, r1, n) {
   if (identical(Z, 1)) {
-    return(lowrank.nmode.prod.r1(lowrank, r1, n))
+    return(nmode.prod.r1(lowrank, r1, n))
   }
 
   # Matrices:
   if (length(lowrank) == 2) {
+    u <- unlist(r1) * lowrank[[-n]]
+
+    if (inherits(Z, "lowrank") && n == 1)
+      return(Matrix::rowSums(lowrank[[1]] * (Z[[1]] %*% crossprod(Z[[2]], u))))
+    if (inherits(Z, "lowrank") && n == 2)
+      return(Matrix::rowSums(lowrank[[2]] * (Z[[2]] %*% crossprod(u, Z[[1]]))))
     if (n == 1)
-      return(Matrix::rowSums(lowrank[[1]]
-                             * (Z %*% (unlist(r1) * lowrank[[2]]))))
+      return(Matrix::rowSums(lowrank[[1]] * (Z %*% u)))
     if (n == 2)
-      return(Matrix::colSums(t(lowrank[[2]])
-                             * (t(unlist(r1) * lowrank[[1]]) %*% Z)))
+      return(Matrix::colSums(t(lowrank[[2]]) * (t(u) %*% Z)))
   }
 
   # 3-dimensional tensors:
@@ -148,12 +167,4 @@ premult.lowrank.nmode.prod.r1 <- function(Z, lowrank, r1, n) {
   }
 
   stop(nmode.ops.error)
-}
-
-premult.nmode.prod.r1 <- function(Z, X, r1, n) {
-  if (is.null(X))
-    return(0)
-  if (inherits(X, "lowrank"))
-    return(premult.lowrank.nmode.prod.r1(Z, X, r1, n))
-  return(fullrank.nmode.prod.r1(Z * X, r1, n))
 }
