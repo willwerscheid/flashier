7f107ce4f69f850e0c765c0f867c85b818c6617f
Jason Willwerscheid <willwerscheid@uchicago.edu>
2019-06-16 13:41:08 -0400

    Implement print method for flash object.

--- R/flash_methods.R
+++ R/flash_methods.R
@@ -7,3 +7,37 @@ fitted.flash <- function(x) {
     stop("S3 method \"fitted\" not yet implemented for tensors.")
   }
 }
+
+#' @export
+print.flash = function(x, ...) {
+  if (x$n.factors == 0) {
+    cat("Flash object with zero factors.\n")
+  } else if (x$n.factors == 1) {
+    cat("Flash object with one factor.\n")
+    cat(sprintf("  Proportion of variance explained: %0.3f\n", x$pve))
+  } else {
+    cat(sprintf("Flash object with %d factors.\n", x$n.factors))
+    if (all(x$pve < 0.001)) {
+      cat("  All factors have PVE < 0.001.")
+    } else {
+      cat("  Proportion of variance explained")
+      if (any(x$pve < 0.001)) {
+        cat("*")
+      }
+      cat(":\n")
+      for (k in 1:length(x$pve)) {
+        if (x$pve[k] > 0.001) {
+          cat(sprintf("    Factor %d: %0.3f\n", k, x$pve[k]))
+        }
+      }
+      if (any(x$pve < 0.001)) {
+        cat(paste("    *Factors with PVE < 0.001 are omitted from this",
+                  "summary.\n"))
+      }
+    }
+  }
+
+  if (!is.na(x$elbo)) {
+    cat(sprintf("  Variational lower bound: %0.3f\n", x$elbo))
+  }
+}
