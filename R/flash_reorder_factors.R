c5628ee5b988e3e3efc81208ebeb47dadc9be359
Jason Willwerscheid <willwerscheid@uchicago.edu>
2022-04-01 20:59:34 -0400

    Ad function flash.reorder.factors

---
+++ R/flash_reorder_factors.R
@@ -0,0 +1,42 @@
+#' Reorder factors in a flash object
+#'
+#' Reorders the factor/loadings pairs in a \code{flash} object.
+#'
+#' @param flash A \code{flash} or \code{flash.fit} object.
+#'
+#' @param kset A vector of integers specifying the new order of the
+#'   factor/loadings pairs. All existing factors must be included in
+#'   \code{kset}; to drop factors, use \code{\link{flash.remove.factors}}.
+#'
+#' @return A \code{\link{flash}} object.
+#'
+#' @export
+#'
+flash.reorder.factors <- function(flash, kset) {
+  flash <- get.fit(flash)
+  must.be.valid.kset(flash, kset)
+
+  if (length(kset) != get.n.factors(flash)) {
+    stop("kset must include all factors.")
+  }
+
+  flash <- set.EF(flash, lapply(get.EF(flash), function(X) X[, kset, drop = FALSE]))
+  flash <- set.EF2(flash, lapply(get.EF2(flash), function(X) X[, kset, drop = FALSE]))
+  flash <- set.KL(flash, lapply(get.KL(flash), `[`, kset))
+  flash <- set.g(flash, get.g(flash)[kset])
+  flash <- set.ebnm.fn(flash, get.ebnm.fn(flash)[kset])
+  flash <- set.is.zero(flash, is.zero(flash)[kset])
+  flash <- set.is.valid(flash, is.valid(flash)[kset])
+
+  fix.dim <- get.fix.dim(flash)
+  fix.dim <- c(fix.dim, rep(list(NULL), length(kset) - length(fix.dim)))
+  fix.idx <- get.fix.idx(flash)
+  fix.idx <- c(fix.idx, rep(list(NULL), length(kset) - length(fix.dim)))
+
+  flash <- set.fix.idx(flash, fix.idx[kset])
+  flash <- set.fix.dim(flash, fix.dim[kset])
+
+  flash <- wrapup.flash(flash, output.lvl = 3L)
+
+  return(flash)
+}
