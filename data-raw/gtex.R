db9c8ea599da8e5042827b26bf7e9c6d64ecbdc8
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-07-13 13:43:35 -0400

    minor changes to data-raw; rerun script

--- data-raw/gtex.R
+++ data-raw/gtex.R
@@ -5,11 +5,12 @@ gtex <- gtex$strong.z
 row.sample <- sample(1:nrow(gtex), 1000)
 gtex <- gtex[row.sample, ]
 
-devtools::use_data(gtex)
+usethis::use_data(gtex, overwrite = TRUE)
 
 
 missing.tissues <- c(7, 8, 19, 20, 24, 25, 31, 34, 37)
 gtex.colors <- read.table("https://github.com/stephenslab/gtexresults/blob/master/data/GTExColors.txt?raw=TRUE", sep = '\t', comment.char = '')[-missing.tissues, 2]
 gtex.colors <- as.character(gtex.colors)
+names(gtex.colors) <- colnames(gtex)
 
-devtools::use_data(gtex.colors)
+usethis::use_data(gtex.colors, overwrite = TRUE)
