4874aa5b09073a6911711896c235bde0b12a19a8
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-07-13 13:41:08 -0400

    Fancy new plot method

--- R/flash_methods.R
+++ R/flash_methods.R
@@ -1,3 +1,245 @@
+#' Plot method for flash objects
+#'
+#' Given a \code{\link{flash}} object, produces up to two figures: one showing
+#'   the proportion of variance explained per factor, and one that plots
+#'   posterior means.
+#'
+#' @param x An object inheriting from class \code{flash}.
+#'
+#' @param incl.scree Whether to include a figure showing the proportion of
+#'   variance explained by each factor/loading pair ("scree plot").
+#'
+#' @param incl.pm Whether to include a figure showing the posterior means for
+#'   either loadings \eqn{L} or factors \eqn{F} (depending on the argument to
+#'   \code{pm.which}). If argument \code{pm.groups}
+#'   is left unspecified, then bar plots will be produced, with each bar
+#'   corresponding to a single value \eqn{\ell_{ik}} or \eqn{f_{jk}}.
+#'   Otherwise, overlapping histograms will be
+#'   produced, with each histogram corresponding to one of the groups
+#'   specified by \code{pm.groups}. One plot is produced for each
+#'   factor/loading pair \eqn{k}.
+#'
+#' @param order.by.pve If \code{TRUE}, then the factor/loading pairs will be
+#'   re-ordered according to proportion of variance explained (from
+#'   highest to lowest).
+#'
+#' @param kset A vector of integers specifying the factor/loading pairs to be
+#'   plotted. If \code{kset = NULL}, then all will be plotted.
+#'
+#' @param pm.which Whether to plot loadings \eqn{L} or factors \eqn{F} in the
+#'   plots of posterior means.
+#'
+#' @param pm.subset A vector of loading indices \eqn{i} or factor indices
+#'   \eqn{j} (depending on the argument to \code{pm.which})
+#'   specifying which values \eqn{\ell_{i \cdot}} or \eqn{f_{j \cdot}} are
+#'   to be shown in the plots of posterior means. If the dataset has row or
+#'   column names, then names rather than indices may be specified. If
+#'   \code{pm.subset = NULL}, then all values will be plotted.
+#'
+#' @param pm.groups A vector specifying the group to which each row
+#'   \eqn{y_{i \cdot}} or column \eqn{y_{\cdot j}} of the data belongs
+#'   (groups may be numeric indices or strings). If \code{pm.groups = NULL},
+#'   then a bar plot of the ungrouped data will be produced (see \code{incl.pm}
+#'   above). Otherwise, a group must be provided for each observation, so that
+#'   the length of \code{pm.groups} is exactly equal to the number of rows or
+#'   columns in the full dataset or, if \code{pm.subset} is specified, in the
+#'   subsetted dataset. If \code{pm.groups} is not \code{NULL}, then a set of
+#'   overlapping histograms will be produced for each factor/loading pair, with
+#'   one histogram per group (again see \code{incl.pm}).
+#'
+#' @param pm.colors A vector specifying a color for each bar (if
+#'   \code{pm.groups = NULL}) or histogram (if \code{pm.groups} is not
+#'   \code{NULL}). Passed directly to argument \code{values} in \strong{ggplot2}
+#'   function \code{\link[ggplot2]{scale_color_manual}}.
+#'
+#' @param ... Additional parameters are ignored.
+#'
+#' @seealso \code{\link{flash}}
+#'
+#' @method plot flash
+#'
+#' @importFrom magrittr `%>%`
+#' @importFrom dplyr mutate left_join
+#' @importFrom ggplot2 ggplot aes geom_point geom_line geom_col
+#' @importFrom ggplot2 geom_histogram geom_vline after_stat
+#' @importFrom ggplot2 scale_x_continuous scale_y_log10
+#' @importFrom ggplot2 scale_fill_manual scale_color_manual
+#' @importFrom ggplot2 scale_fill_brewer scale_color_brewer
+#' @importFrom ggplot2 labs guides facet_wrap
+#' @importFrom ggplot2 theme theme_minimal element_blank
+#' @importFrom tibble rownames_to_column
+#' @importFrom stringr str_remove
+#' @importFrom tidyr pivot_longer
+#' @importFrom grDevices devAskNewPage
+#'
+#' @export
+#'
+plot.flash <- function(x,
+                       incl.scree = TRUE,
+                       incl.pm = TRUE,
+                       order.by.pve = TRUE,
+                       kset = NULL,
+                       pm.which = c("factors", "loadings"),
+                       pm.subset = NULL,
+                       pm.groups = NULL,
+                       pm.colors = NULL,
+                       ...) {
+  pm.which <- match.arg(pm.which)
+
+  if (x$n.factors == 0) {
+    stop("Flash object has no factors, so there is nothing to plot.")
+  }
+  if (!incl.scree && !incl.pm) {
+    stop("Include either the scree plot (via argument incl.scree) or the plot ",
+         " of posterior means (via incl.pm).")
+  }
+
+  # Scree plot:
+  all.plots <- list()
+  if (is.null(kset)) {
+    kset <- 1:x$n.factors
+  } else {
+    must.be.valid.kset(get.fit(x), kset)
+  }
+
+  pve.df <- data.frame(
+    k = kset,
+    pve = x$pve[kset],
+    k.order = kset
+  )
+  if (order.by.pve) {
+    pve.df <- pve.df %>%
+      mutate(k.order = rank(-pve))
+  }
+
+  if (incl.scree) {
+    # Include ~4 x-axis ticks with the distance a multiple of 5:
+    K <- length(kset)
+    if (K < 5) {
+      tick_dist <- 1
+    } else if (K < 10) {
+      tick_dist <- 2
+    } else {
+      tick_dist <- max(1, floor(K / 20)) * 5
+    }
+    p1 <- ggplot(pve.df) +
+      geom_line(aes(x = k.order, y = pve), color = "grey") +
+      geom_point(aes(x = k.order, y = pve), color = "dodgerblue") +
+      scale_x_continuous(breaks = seq(tick_dist, K, by = tick_dist)) +
+      scale_y_log10() +
+      labs(title = "Proportion of variance explained per factor") +
+      labs(x = "k", y = "PVE") +
+      theme_minimal()
+    all.plots[["scree"]] <- p1
+  }
+
+  if (incl.pm) {
+    if (pm.which == "factors") {
+      which.dim <- "column"
+      val <- ldf(x, type = "i")$F
+      colnames(val) <- 1:x$n.factors
+    } else {
+      which.dim <- "row"
+      val <- ldf(x, type = "i")$L
+      colnames(val) <- 1:x$n.factors
+    }
+    if (is.null(pm.subset)) {
+      val <- val[, kset, drop = FALSE]
+    } else {
+      if (!all(pm.subset %in% 1:nrow(val)) &&
+          !all(pm.subset %in% rownames(val))) {
+        stop("Argument to pm.subset must be a vector of valid ", which.dim,
+             " indices or valid ", which.dim, " names.")
+      }
+      val <- val[pm.subset, kset, drop = FALSE]
+    }
+
+    pm.df <- data.frame(val = val) %>%
+      rownames_to_column(var = "Name") %>%
+      pivot_longer(-Name, names_to = "k", values_to = "val") %>%
+      mutate(k = as.numeric(str_remove(k, "val."))) %>%
+      left_join(pve.df, by = "k")
+
+    if (is.null(pm.groups)) {
+      if (nrow(val) > 100) {
+        warning("Consider setting argument pm.groups to produce a more",
+                " readable plot.")
+      }
+      if (is.null(pm.colors)) {
+        p2 <- ggplot(pm.df) +
+          geom_col(aes(x = Name, y = val), fill = "dodgerblue")
+      } else {
+        if (length(pm.colors) < nrow(val)) {
+          stop("Argument to pm.colors must be a vector consisting of one ",
+               "color for each ", which.dim, " in the data (or each ",
+               "subsetted ", which.dim, ").")
+        }
+        p2 <- ggplot(pm.df) +
+          geom_col(aes(x = Name, y = val, fill = Name)) +
+          scale_fill_manual(values = pm.colors) +
+          guides(fill = "none")
+      }
+      p2 <- p2 +
+        facet_wrap(~k.order) +
+        theme_minimal() +
+        theme(axis.text = element_blank()) +
+        labs(title = paste0("Posterior means for ", pm.which)) +
+        labs(x = "", y = "")
+    } else {
+      if (length(pm.groups) != nrow(val)) {
+        stop("Argument to pm.groups must be a vector with length equal to ",
+             "the number of ", which.dim, "s in the data (or the number of ",
+             "subsetted ", which.dim, "s.")
+      }
+      pm.df <- pm.df %>%
+        mutate(grp = rep(factor(pm.groups), each = length(kset)))
+      p2 <- ggplot(pm.df) +
+        geom_histogram(
+          aes(x = val, y = after_stat(density), color = grp, fill = grp),
+          position = "identity", bins = 20, alpha = 0.5
+        )
+      if (is.null(pm.colors)) {
+        if (length(unique(pm.groups)) > 9) {
+          warning("Consider reducing the number of groups or defining a",
+                  " custom color palette to produce a more readable plot.")
+        } else {
+          p2 <- p2 +
+            scale_color_brewer(palette = "Set1") +
+            scale_fill_brewer(palette = "Set1")
+        }
+      } else {
+        if (length(pm.colors) < length(unique(pm.groups))) {
+          stop("Argument to pm.colors must be a vector consisting of one ",
+               "color for each unique group in argument pm.groups.")
+        }
+        p2 <- p2 +
+          scale_color_manual(values = pm.colors) +
+          scale_fill_manual(values = pm.colors)
+      }
+      p2 <- p2 +
+        facet_wrap(~k.order, scales = "free_y") +
+        geom_vline(xintercept = 0, color = "darkgrey") +
+        theme_minimal() +
+        theme(axis.text = element_blank()) +
+        guides(color = "none") +
+        labs(title = paste0("Posterior means for ", pm.which)) +
+        labs(x = "", y = "", fill = "Group")
+    }
+
+    all.plots[["pm"]] <- p2
+  }
+
+  if (length(all.plots) == 1) {
+    return(all.plots[[1]])
+  } else {
+    oask <- devAskNewPage(TRUE)
+    print(all.plots[["scree"]])
+    print(all.plots[["pm"]])
+    devAskNewPage(oask)
+    return(invisible())
+  }
+}
+
 #' Fitted method for flash objects
 #'
 #' Given a \code{flash} object, returns the "fitted values"
@@ -8,6 +250,8 @@
 #' @param ... Additional parameters are ignored.
 #'
 #' @seealso \code{\link{flash}}
+#'
+#' @method fitted flash
 #'
 #' @export
 #'
@@ -25,6 +269,8 @@ fitted.flash <- function(object, ...) {
 #' @param ... Additional parameters are ignored.
 #'
 #' @seealso \code{\link{flash}}
+#'
+#' @method fitted flash.fit
 #'
 #' @export
 #'
