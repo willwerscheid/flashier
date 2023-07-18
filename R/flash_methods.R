024b7e3eb785e45ad2b54ffa9893710038c93fe2
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-07-18 12:07:27 -0400

    update interface and docs for flash methods

--- R/flash_methods.R
+++ R/flash_methods.R
@@ -1,60 +1,64 @@
 #' Plot method for flash objects
 #'
 #' Given a \code{\link{flash}} object, produces up to two figures: one showing
-#'   the proportion of variance explained per factor, and one that plots
-#'   posterior means.
+#'   the proportion of variance explained per factor/loadings pair, and one that
+#'   plots posterior means for either factors or loadings (depending on the
+#'   argument to parameter \code{pm_which}).
 #'
 #' @param x An object inheriting from class \code{flash}.
 #'
-#' @param incl.scree Whether to include a figure showing the proportion of
-#'   variance explained by each factor/loadings pair ("scree plot").
+#' @param include_scree Whether to include a figure ("scree plot") showing the
+#'   proportion of variance explained by each factor/loadings pair.
 #'
-#' @param incl.pm Whether to include a figure showing the posterior means for
+#' @param include_pm Whether to include a figure showing the posterior means for
 #'   either loadings \eqn{L} or factors \eqn{F} (depending on the argument to
-#'   \code{pm.which}). If argument \code{pm.groups}
+#'   parameter \code{pm_which}). One plot panel is produced for each
+#'   factor/loadings pair \eqn{k}. If argument \code{pm_groups}
 #'   is left unspecified, then bar plots will be produced, with each bar
 #'   corresponding to a single value \eqn{\ell_{ik}} or \eqn{f_{jk}}.
 #'   Otherwise, overlapping histograms will be
 #'   produced, with each histogram corresponding to one of the groups
-#'   specified by \code{pm.groups}. One plot is produced for each
-#'   factor/loadings pair \eqn{k}.
+#'   specified by \code{pm_groups}.
 #'
-#' @param order.by.pve If \code{TRUE}, then the factor/loadings pairs will be
+#' @param order_by_pve If \code{TRUE}, then the factor/loadings pairs will be
 #'   re-ordered according to proportion of variance explained (from
 #'   highest to lowest).
 #'
 #' @param kset A vector of integers specifying the factor/loadings pairs to be
 #'   plotted. If \code{kset = NULL}, then all will be plotted.
 #'
-#' @param pm.which Whether to plot loadings \eqn{L} or factors \eqn{F} in the
+#' @param pm_which Whether to plot loadings \eqn{L} or factors \eqn{F} in the
 #'   plots of posterior means.
+#'   This parameter is ignored when \code{include_pm = FALSE}.
 #'
-#' @param pm.subset A vector of loading indices \eqn{i} or factor indices
-#'   \eqn{j} (depending on the argument to \code{pm.which})
+#' @param pm_subset A vector of row indices \eqn{i} or column indices
+#'   \eqn{j} (depending on the argument to \code{pm_which})
 #'   specifying which values \eqn{\ell_{i \cdot}} or \eqn{f_{j \cdot}} are
 #'   to be shown in the plots of posterior means. If the dataset has row or
 #'   column names, then names rather than indices may be specified. If
-#'   \code{pm.subset = NULL}, then all values will be plotted.
+#'   \code{pm_subset = NULL}, then all values will be plotted.
+#'   This parameter is ignored when \code{include_pm = FALSE}.
 #'
-#' @param pm.groups A vector specifying the group to which each row
-#'   \eqn{y_{i \cdot}} or column \eqn{y_{\cdot j}} of the data belongs
-#'   (groups may be numeric indices or strings). If \code{pm.groups = NULL},
-#'   then a bar plot of the ungrouped data will be produced (see \code{incl.pm}
-#'   above). Otherwise, a group must be provided for each observation, so that
-#'   the length of \code{pm.groups} is exactly equal to the number of rows or
-#'   columns in the full dataset or, if \code{pm.subset} is specified, in the
-#'   subsetted dataset. If \code{pm.groups} is not \code{NULL}, then a set of
-#'   overlapping histograms will be produced for each factor/loadings pair, with
-#'   one histogram per group (again see \code{incl.pm}).
+#' @param pm_groups A vector specifying the group to which each row of the data
+#'   \eqn{y_{i \cdot}} or column \eqn{y_{\cdot j}} belongs
+#'   (groups may be numeric indices or strings). If \code{pm_groups = NULL},
+#'   then a bar plot of the ungrouped data is produced (see \code{include_pm}
+#'   above). Otherwise, a group must be provided for each plotted row \eqn{i} or
+#'   column \eqn{j}, so that
+#'   the length of \code{pm_groups} is exactly equal to the number of rows or
+#'   columns in the full dataset or, if \code{pm_subset} is specified, in the
+#'   subsetted dataset. When \code{pm_groups} is not \code{NULL}, a set of
+#'   overlapping histograms is produced for each factor/loadings pair, with
+#'   one histogram per group (again see \code{include_pm}).
+#'   This parameter is ignored when \code{include_pm = FALSE}.
 #'
-#' @param pm.colors A vector specifying a color for each bar (if
-#'   \code{pm.groups = NULL}) or histogram (if \code{pm.groups} is not
-#'   \code{NULL}). Passed directly to argument \code{values} in \strong{ggplot2}
+#' @param pm_colors A vector specifying a color for each bar (if
+#'   \code{pm_groups = NULL}) or histogram (if \code{pm_groups} is not
+#'   \code{NULL}). Passed directly to parameter \code{values} in \strong{ggplot2}
 #'   function \code{\link[ggplot2]{scale_color_manual}}.
+#'   This parameter is ignored when \code{include_pm = FALSE}.
 #'
 #' @param ... Additional parameters are ignored.
-#'
-#' @seealso \code{\link{flash}}
 #'
 #' @method plot flash
 #'
@@ -76,23 +80,23 @@
 #' @export
 #'
 plot.flash <- function(x,
-                       incl.scree = TRUE,
-                       incl.pm = TRUE,
-                       order.by.pve = TRUE,
+                       include_scree = TRUE,
+                       include_pm = TRUE,
+                       order_by_pve = TRUE,
                        kset = NULL,
-                       pm.which = c("factors", "loadings"),
-                       pm.subset = NULL,
-                       pm.groups = NULL,
-                       pm.colors = NULL,
+                       pm_which = c("factors", "loadings"),
+                       pm_subset = NULL,
+                       pm_groups = NULL,
+                       pm_colors = NULL,
                        ...) {
-  pm.which <- match.arg(pm.which)
+  pm_which <- match.arg(pm_which)
 
   if (x$n.factors == 0) {
     stop("Flash object has no factors, so there is nothing to plot.")
   }
-  if (!incl.scree && !incl.pm) {
-    stop("Include either the scree plot (via argument incl.scree) or the plot ",
-         " of posterior means (via incl.pm).")
+  if (!include_scree && !include_pm) {
+    stop("Include either the scree plot (via argument include_scree) or the plot ",
+         " of posterior means (via include_pm).")
   }
 
   # Bind variables to get rid of annoying R CMD check note:
@@ -111,12 +115,12 @@ plot.flash <- function(x,
     pve = x$pve[kset],
     k.order = kset
   )
-  if (order.by.pve) {
+  if (order_by_pve) {
     pve.df <- pve.df %>%
       mutate(k.order = rank(-pve))
   }
 
-  if (incl.scree) {
+  if (include_scree) {
     # Include ~4 x-axis ticks with the distance a multiple of 5:
     K <- length(kset)
     if (K < 5) {
@@ -137,8 +141,8 @@ plot.flash <- function(x,
     all.plots[["scree"]] <- p1
   }
 
-  if (incl.pm) {
-    if (pm.which == "factors") {
+  if (include_pm) {
+    if (pm_which == "factors") {
       which.dim <- "column"
       val <- ldf(x, type = "i")$F
       colnames(val) <- 1:x$n.factors
@@ -147,15 +151,15 @@ plot.flash <- function(x,
       val <- ldf(x, type = "i")$L
       colnames(val) <- 1:x$n.factors
     }
-    if (is.null(pm.subset)) {
+    if (is.null(pm_subset)) {
       val <- val[, kset, drop = FALSE]
     } else {
-      if (!all(pm.subset %in% 1:nrow(val)) &&
-          !all(pm.subset %in% rownames(val))) {
-        stop("Argument to pm.subset must be a vector of valid ", which.dim,
+      if (!all(pm_subset %in% 1:nrow(val)) &&
+          !all(pm_subset %in% rownames(val))) {
+        stop("Argument to pm_subset must be a vector of valid ", which.dim,
              " indices or valid ", which.dim, " names.")
       }
-      val <- val[pm.subset, kset, drop = FALSE]
+      val <- val[pm_subset, kset, drop = FALSE]
     }
 
     pm.df <- data.frame(val = val) %>%
@@ -164,46 +168,46 @@ plot.flash <- function(x,
       mutate(k = as.numeric(str_remove(k, "val."))) %>%
       left_join(pve.df, by = "k")
 
-    if (is.null(pm.groups)) {
+    if (is.null(pm_groups)) {
       if (nrow(val) > 100) {
-        warning("Consider setting argument pm.groups to produce a more",
+        warning("Consider setting argument pm_groups to produce a more",
                 " readable plot.")
       }
-      if (is.null(pm.colors)) {
+      if (is.null(pm_colors)) {
         p2 <- ggplot(pm.df) +
           geom_col(aes(x = Name, y = val), fill = "dodgerblue")
       } else {
-        if (length(pm.colors) < nrow(val)) {
-          stop("Argument to pm.colors must be a vector consisting of one ",
+        if (length(pm_colors) < nrow(val)) {
+          stop("Argument to pm_colors must be a vector consisting of one ",
                "color for each ", which.dim, " in the data (or each ",
                "subsetted ", which.dim, ").")
         }
         p2 <- ggplot(pm.df) +
           geom_col(aes(x = Name, y = val, fill = Name)) +
-          scale_fill_manual(values = pm.colors) +
+          scale_fill_manual(values = pm_colors) +
           guides(fill = "none")
       }
       p2 <- p2 +
         facet_wrap(~k.order) +
         theme_minimal() +
         theme(axis.text = element_blank()) +
-        labs(title = paste0("Posterior means for ", pm.which)) +
+        labs(title = paste0("Posterior means for ", pm_which)) +
         labs(x = "", y = "")
     } else {
-      if (length(pm.groups) != nrow(val)) {
-        stop("Argument to pm.groups must be a vector with length equal to ",
+      if (length(pm_groups) != nrow(val)) {
+        stop("Argument to pm_groups must be a vector with length equal to ",
              "the number of ", which.dim, "s in the data (or the number of ",
              "subsetted ", which.dim, "s.")
       }
       pm.df <- pm.df %>%
-        mutate(grp = rep(factor(pm.groups), each = length(kset)))
+        mutate(grp = rep(factor(pm_groups), each = length(kset)))
       p2 <- ggplot(pm.df) +
         geom_histogram(
           aes(x = val, y = after_stat(density), color = grp, fill = grp),
           position = "identity", bins = 20, alpha = 0.5
         )
-      if (is.null(pm.colors)) {
-        if (length(unique(pm.groups)) > 9) {
+      if (is.null(pm_colors)) {
+        if (length(unique(pm_groups)) > 9) {
           warning("Consider reducing the number of groups or defining a",
                   " custom color palette to produce a more readable plot.")
         } else {
@@ -212,13 +216,13 @@ plot.flash <- function(x,
             scale_fill_brewer(palette = "Set1")
         }
       } else {
-        if (length(pm.colors) < length(unique(pm.groups))) {
-          stop("Argument to pm.colors must be a vector consisting of one ",
-               "color for each unique group in argument pm.groups.")
+        if (length(pm_colors) < length(unique(pm_groups))) {
+          stop("Argument to pm_colors must be a vector consisting of one ",
+               "color for each unique group in argument pm_groups.")
         }
         p2 <- p2 +
-          scale_color_manual(values = pm.colors) +
-          scale_fill_manual(values = pm.colors)
+          scale_color_manual(values = pm_colors) +
+          scale_fill_manual(values = pm_colors)
       }
       p2 <- p2 +
         facet_wrap(~k.order, scales = "free_y") +
@@ -226,7 +230,7 @@ plot.flash <- function(x,
         theme_minimal() +
         theme(axis.text = element_blank()) +
         guides(color = "none") +
-        labs(title = paste0("Posterior means for ", pm.which)) +
+        labs(title = paste0("Posterior means for ", pm_which)) +
         labs(x = "", y = "", fill = "Group")
     }
 
@@ -246,14 +250,12 @@ plot.flash <- function(x,
 
 #' Fitted method for flash objects
 #'
-#' Given a \code{flash} object, returns the "fitted values"
+#' Given a \code{\link{flash}} object, returns the "fitted values"
 #'   \eqn{E(LF') = E(L) E(F)'}.
 #'
 #' @param object An object inheriting from class \code{flash}.
 #'
 #' @param ... Additional parameters are ignored.
-#'
-#' @seealso \code{\link{flash}}
 #'
 #' @importFrom stats fitted
 #' @method fitted flash
@@ -261,26 +263,24 @@ plot.flash <- function(x,
 #' @export
 #'
 fitted.flash <- function(object, ...) {
-  return(fitted.flash.fit(get.fit(object)))
+  return(fitted.flash_fit(get.fit(object)))
 }
 
 #' Fitted method for flash fit objects
 #'
-#' Given a \code{flash.fit} object, returns the "fitted values"
+#' Given a \code{\link{flash_fit}} object, returns the "fitted values"
 #'   \eqn{E(LF') = E(L) E(F)'}.
 #'
-#' @param object An object inheriting from class \code{flash.fit}.
+#' @param object An object inheriting from class \code{flash_fit}.
 #'
 #' @param ... Additional parameters are ignored.
 #'
-#' @seealso \code{\link{flash}}
-#'
 #' @importFrom stats fitted
-#' @method fitted flash.fit
+#' @method fitted flash_fit
 #'
 #' @export
 #'
-fitted.flash.fit <- function(object, ...) {
+fitted.flash_fit <- function(object, ...) {
   if (get.n.factors(object) == 0) {
     warning("Flash object does not have any factors.")
     return(matrix(data = 0, nrow = nrow(object$Y), ncol = ncol(object$Y)))
@@ -294,12 +294,10 @@ fitted.flash.fit <- function(object, ...) {
 
 #' Residuals method for flash objects
 #'
-#' Given a \code{flash} object, returns the expected residuals
+#' Given a \code{\link{flash}} object, returns the expected residuals
 #'   \eqn{Y - E(LF') = Y - E(L) E(F)'}.
 #'
 #' @inheritParams fitted.flash
-#'
-#' @seealso \code{\link{flash}}
 #'
 #' @importFrom stats residuals
 #' @method residuals flash
@@ -307,24 +305,22 @@ fitted.flash.fit <- function(object, ...) {
 #' @export
 #'
 residuals.flash <- function(object, ...) {
-  return(residuals.flash.fit(get.fit(object)))
+  return(residuals.flash_fit(get.fit(object)))
 }
 
 #' Residuals method for flash fit objects
 #'
-#' Given a \code{flash.fit} object, returns the expected residuals
+#' Given a \code{\link{flash_fit}} object, returns the expected residuals
 #'   \eqn{Y - E(LF') = Y - E(L) E(F)'}.
 #'
-#' @inheritParams fitted.flash.fit
-#'
-#' @seealso \code{\link{flash}}
+#' @inheritParams fitted.flash_fit
 #'
 #' @importFrom stats residuals
-#' @method residuals flash.fit
+#' @method residuals flash_fit
 #'
 #' @export
 #'
-residuals.flash.fit <- function(object, ...) {
+residuals.flash_fit <- function(object, ...) {
   if (uses.R(object)) {
     R <- get.R(object)
   } else {
@@ -340,7 +336,7 @@ residuals.flash.fit <- function(object, ...) {
 
 #' LDF method for flash and flash fit objects
 #'
-#' Given a \code{flash} or \code{flash.fit} object, returns the LDF
+#' Given a \code{\link{flash}} or \code{\link{flash_fit}} object, returns the LDF
 #'   decomposition \eqn{Y \approx LDF'}.
 #'
 #' When the prior families \eqn{G_\ell^{(k)}} and \eqn{G_f^{(k)}} are closed
@@ -354,14 +350,17 @@ residuals.flash.fit <- function(object, ...) {
 #'   1-norms, 2-norms, or infinity norms are equal to 1.
 #'
 #' @param object An object inheriting from class \code{flash} or
-#'   \code{flash.fit}.
+#'   \code{flash_fit}.
 #'
 #' @param type Takes identical arguments to function \code{\link[base]{norm}}. Use
 #'   \code{"f"} or \code{"2"} for the 2-norm (Euclidean norm); \code{"o"} or
 #'   \code{"1"} for the 1-norm (taxicab norm); and \code{"i"} or \code{"m"} for
 #'   the infinity norm (maximum norm).
 #'
-#' @seealso \code{\link{flash}}
+#' @return A list with fields \code{L}, \code{D}, and \code{F}, each of which
+#'   contains one of the matrices in the decomposition \eqn{Y \approx LDF'},
+#'   with the columns of \eqn{L} and \eqn{F} scaled according to
+#'   the argument to parameter \code{type}.
 #'
 #' @export
 #'
@@ -369,23 +368,23 @@ ldf <- function(object, type) {
   UseMethod("ldf", object)
 }
 
-#' @describeIn ldf LDF decomposition for flash objects
+#' @describeIn ldf LDF decomposition for \code{\link{flash}} objects
 #'
 #' @method ldf flash
 #'
 #' @export
 #'
 ldf.flash <- function(object, type = "f") {
-  return(ldf.flash.fit(get.fit(object), type = type))
+  return(ldf.flash_fit(get.fit(object), type = type))
 }
 
-#' @describeIn ldf LDF decomposition for flash fit objects
+#' @describeIn ldf LDF decomposition for \code{\link{flash_fit}} objects
 #'
-#' @method ldf flash.fit
+#' @method ldf flash_fit
 #'
 #' @export
 #'
-ldf.flash.fit <- function(object, type = "f") {
+ldf.flash_fit <- function(object, type = "f") {
   type <- tolower(type)
   if (type == "2") {
     type <- "f"
