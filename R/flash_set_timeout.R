76a0825c8d5e4b1e677ce31a4a27866d314a7564
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-07-20 15:39:53 -0400

    docs for flash_set_ and flash_clear_timeout; minor edit to flash_set_verbose docs

--- R/flash_set_timeout.R
+++ R/flash_set_timeout.R
@@ -1,3 +1,40 @@
+#' Set timeout
+#'
+#' Used in a \code{\link{flash}} pipeline to set a maximum amount of fitting
+#'   time. Note that timeout conditions are only checked during greedy fits
+#'   and backfits, so that the total amount of fitting time can exceed the time
+#'   set by \code{flash_set_timeout} (especially if, for example, there is a
+#'   nullcheck involving many factor/loading pairs). Also note that timeout
+#'   conditions must be cleared using function \code{\link{flash_clear_timeout}}
+#'   before any re-fitting is attempted.
+#'
+#' @param flash A \code{flash} or \code{flash_fit} object.
+#'
+#' @param tim A numeric value giving the maximum amount of fitting time, with
+#'   the units of time specified by parameter \code{units}.
+#'
+#' @param units The units of time according to which parameter \code{tim} is to
+#'   be interpreted.
+#'
+#' @return The \code{\link{flash}} object from argument \code{flash}, with the
+#'   timeout settings reflected in updates to the "internal" \code{flash_fit}
+#'   object. These settings will persist across all subsequent calls to
+#'   \code{flash_xxx} functions until they are modified either by
+#'   \code{\link{flash_clear_timeout}} or by another call to
+#'   \code{flash_set_timeout}.
+#'
+#' @examples
+#' data(gtex)
+#'
+#' fl <- flash_init(gtex) %>%
+#'   flash_set_timeout(1, "secs") %>%
+#'   flash_greedy(Kmax = 30) %>%
+#'   flash_backfit() %>%
+#'   flash_nullcheck() %>%
+#'   flash_clear_timeout() # Always clear timeout at the end of a pipeline.
+#'
+#' @export
+#'
 flash_set_timeout <- function(flash,
                               tim,
                               units = c("hours", "secs", "mins", "days", "weeks")) {
@@ -15,6 +52,18 @@ flash_set_timeout <- function(flash,
   return(flash)
 }
 
+#' Set timeout
+#'
+#' Used in a \code{\link{flash}} pipeline to clear timeout conditions set using
+#'   \code{\link{flash_set_timeout}}.
+#'
+#' @param flash A \code{flash} or \code{flash_fit} object.
+#'
+#' @return The \code{\link{flash}} object from argument \code{flash}, with
+#'   timeout settings cleared.
+#'
+#' @export
+#'
 flash_clear_timeout <- function(flash) {
   fit <- get.fit(flash)
 
