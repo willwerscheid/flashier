acc868f0575da629de0f7ecaf747178cc22d7834
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-12-13 22:16:59 -0500

    export sptensor methods

--- R/data_sptensor.R
+++ R/data_sptensor.R
@@ -3,18 +3,22 @@
 # dimension (ie, for an m x n x p tensor with p < m and p < n, represent the
 # tensor as a p-list of m x n matrices).
 #
+#' @exportS3Method as.fullrank sptensor
 as.fullrank.sptensor <-function(x, ...) {
   return(sapply(x, as.matrix, simplify = "array"))
 }
 
+#' @exportS3Method get.data.dims sptensor
 get.data.dims.sptensor <- function(x, ...) {
   return(c(dim(x[[1]]), length(x)))
 }
 
+#' @exportS3Method get.data.dimnames sptensor
 get.data.dimnames.sptensor <- function(x, ...) {
   return(c(dimnames(x[[1]]), list(names(x))))
 }
 
+#' @exportS3Method nmode.prod.vec sptensor
 nmode.prod.vec.sptensor <- function(x, v, n, ...) {
   if (n == 1 && identical(v, 1))
     return(Reduce(cbind, lapply(x, colSums)))
@@ -29,6 +33,7 @@ nmode.prod.vec.sptensor <- function(x, v, n, ...) {
   }
 }
 
+#' @exportS3Method sq.nmode.prod.r1 sptensor
 sq.nmode.prod.r1.sptensor <- function(x, r1, n, ...) {
   x2 <- lapply(x, `^`, 2)
   class(x2) <- "sptensor"
