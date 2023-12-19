c754d70cc3dec4ea00574667d4c1176fc2248d29
Jason Willwerscheid <willwerscheid@uchicago.edu>
2023-12-19 18:09:21 -0500

    add premult.nmode.prod.r1 for sptensor class, fix weird cbind bug, export sptensor methods

--- R/data_sptensor.R
+++ R/data_sptensor.R
@@ -20,17 +20,53 @@ get.data.dimnames.sptensor <- function(x, ...) {
 
 #' @exportS3Method nmode.prod.vec sptensor
 nmode.prod.vec.sptensor <- function(x, v, n, ...) {
+  # cbind names the first column, which can throw weird errors.
+  cbind0 <- function(...) cbind(..., deparse.level = 0)
   if (n == 1 && identical(v, 1))
-    return(Reduce(cbind, lapply(x, colSums)))
+    return(Reduce(cbind0, lapply(x, colSums)))
   if (n == 2 && identical(v, 1))
-    return(Reduce(cbind, lapply(x, rowSums)))
+    return(Reduce(cbind0, lapply(x, rowSums)))
   if (n == 1 || n == 2)
-    return(Reduce(cbind, lapply(x, nmode.prod.vec, v = v, n = n)))
+    return(Reduce(cbind0, lapply(x, nmode.prod.vec, v = v, n = n)))
   if (n == 3 && identical(v, 1))
     return(Reduce(`+`, x))
   if (n == 3) {
     return(Reduce(`+`, mapply(`*`, x, v)))
   }
+}
+
+#' @exportS3Method premult.nmode.prod.r1 sptensor
+premult.nmode.prod.r1.sptensor <- function(x, lr, r1, n, ...) {
+  if (is.null(lr))
+    return(0)
+
+  if (inherits(lr, "lowrank")) {
+    ns <- (1:3)[-n]
+    n1 <- ns[1]
+    n2 <- ns[2]
+    K <- ncol(lr[[1]])
+
+    # Sum over third (or second) index. Get a length-K list of m_1 x m_2
+    #   (or m_1 x m_3) sparse matrices.
+    tmp <- apply(lr[[n2]], 2, function(lr_k) {
+      nmode.prod.vec(x, r1[[2]] * lr_k, n2)
+    }, simplify = FALSE)
+    # Multiply each matrix by an m_1 (or m_2) vector using broadcasting and
+    #   take sums; gives m_2/m_3 x K (or m_1 x K) matrix.
+    if (n1 == 1) {
+      tmp <- sapply(1:K, function(k) {
+        colSums(tmp[[k]] * lr[[n1]][, k] * r1[[1]])
+      })
+    } else { # n1 == 2, so n2 == 3
+      tmp <- sapply(1:K, function(k) {
+        colSums(t(tmp[[k]]) * lr[[n1]][, k] * r1[[1]])
+      })
+    }
+    return(rowSums(tmp * lr[[n]]))
+  }
+
+  # else lr is full-rank (TODO: shouldn't get here):
+  return(nmode.prod.r1(as.fullrank(x) * lr, r1, n))
 }
 
 #' @exportS3Method sq.nmode.prod.r1 sptensor
